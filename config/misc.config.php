685bc26b5a04126c28eaef3e5f82256598eeb55b
Valentin Day <valentin.day@colibris.xyz>
2021-07-26 09:31:54 +0200

    Add env config for default language / locale

--- config/misc.config.php
+++ config/misc.config.php
@@ -1,5 +1,7 @@
 <?php
 $CONFIG = array (
+  'default_language' => getenv('DEFAULT_LANGUAGE'),
+  'default_locale' => getenv('DEFAULT_LOCALE'),
   'default_phone_region' => getenv('DEFAULT_PHONE_REGION'),
   'trashbin_retention_obligation' => getenv('TRASHBIN_RETENTION_OBLIGATION') ?: 'auto'
 );
